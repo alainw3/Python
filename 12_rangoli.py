8b68e20fdee71fd25ef5566dcad4c6eb8104ab07
Alain Razakarivony <alainw3@gmail.com>
2025-12-15 11:24:05 +0100

    rangoli

--- 12_rangoli.py
+++ 12_rangoli.py
@@ -1,15 +1,23 @@
 def print_rangoli(size):
     # your code goes here
+    #size =3 
+    dep = size+64
+
     for _ in range(size):
-        #print("a".center(size+2,"-"))
         x = _+1
-        line =(*(chr((size+_-i-1)+65).lower() for i in range(x-1,0,-1)),
-               *(chr((size+_-i-1)+65).lower() for i in range(x       )),
-              )
+   
+        line =(*(chr((dep-_+i)).lower() for i in range(x-1,0,-1)),
+               *(chr((dep-_+i)).lower() for i in range(x       )),
+        )
         print("-".join(line).center((size*2+(size-1)*2)-1,"-"))
-             
+        
+    for _ in range(size-2,-1,-1):
+        x = _+1
+   
+        line =(*(chr((dep-_+i)).lower() for i in range(x-1,0,-1)),
+               *(chr((dep-_+i)).lower() for i in range(x       )),
+        )
+        print("-".join(line).center((size*2+(size-1)*2)-1,"-"))
 
 
 if __name__ == '__main__':
-    n = int(input())
-    print_rangoli(n)
